20e6d2fe245273932787369e379df8ac5640d0cf
Ken <kentflores0000@gmail.com>
2026-05-10 07:02:29 +0800

    deleted ts

--- web/middleware.ts
+++
@@ -1,9 +0,0 @@
-// middleware.ts
-
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
-export function middleware(request: NextRequest) {
-  // Optional: Log or modify request/response here
-  return NextResponse.next(); // allows the request to continue
-}
