ff7dbdfb95cd46ac94383bbd7dff996241ea321a
Ken <kentflores0000@gmail.com>
2026-05-24 23:35:21 +0800

    another new projects

--- web/app/projects/page.tsx
+++ web/app/projects/page.tsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 export default function Projects() {
     const pasaHeroUrl =
         "https://pasa-hero-landing-page.vercel.app/?fbclid=IwZXh0bgNhZW0CMTAAYnJpZBExQ09pTWZLRTV4TWpxVGRaZ3NydGMGYXBwX2lkEDIyMjAzOTE3ODgyMDA4OTIAAR4B14nUjhICkmLZVAyttTMYCgGNyKCq1f2hVDRtOKDDaC3fT4QtqBAPBZ1auA_aem_0QqxyCmG9fY4ppaRrs3JeA";
-
+    const PasaHero_driverURL =
+    "https://pasahero-driver.vercel.app/";
     return (
         <div className="w-full overflow-hidden relative">
             <main className="min-h-[calc(100vh-3.5rem)] md:min-h-screen flex items-center justify-center px-6 py-16">
@@ -20,6 +21,14 @@ export default function Projects() {
                         className="mt-8 inline-flex rounded-xl border border-white/20 bg-white/10 px-6 py-3 text-lg font-semibold text-white transition hover:bg-white/20"
                     >
                         PASAHERO APP
+                    </Link>
+                    <Link
+                        href={PasaHero_driverURL}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="mt-8 inline-flex rounded-xl border border-white/20 bg-white/10 px-6 py-3 text-lg font-semibold text-white transition hover:bg-white/20"
+                    >
+                        PASAHERO DRIVER
                     </Link>
                 </div>
             </main>
